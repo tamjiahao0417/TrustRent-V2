6c554dcbc15cf4e6e9d031be0401392e351c57c6
tamjiahao0417 <tamhao@graduate.utm.my>
2026-06-30 23:09:13 +0800

    Revert "Update cors.php"
    This reverts commit 62dfe2ec3e368aa43bfba5b1ecef338d4400b612.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://your-angular-site-name.onrender.com'],
+    'allowed_origins' => ['http://localhost:4200'],
 
     'allowed_origins_patterns' => [],
 
