2da2601e0e0fcfc308a67e6639c52fbddfb89ef4
tamjiahao0417 <tamhao@graduate.utm.my>
2026-06-30 23:12:32 +0800

    Revert "Update database.php"
    This reverts commit e8f06bc43270b5f000d980ffd6d11da1a5794df0.

--- backend/config/database.php
+++ backend/config/database.php
@@ -86,15 +86,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'aws-1-ap-southeast-1.pooler.supabase.com'),
-            'port' => env('DB_PORT', '6543'),
-            'database' => env('DB_DATABASE', 'postgres'),
-            'username' => env('DB_USERNAME', 'postgres.lfwshxjapqlrurfqnadq'),
+            //'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
+            'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'require', // <--- ADD THIS LINE
         ],
 
         'sqlsrv' => [
