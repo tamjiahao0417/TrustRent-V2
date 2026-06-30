cefcb5ac5241356a172a20f785f3e2ffc93aaf2a
tamjiahao0417 <tamhao@graduate.utm.my>
2026-06-30 23:13:02 +0800

    Revert "deploy"
    This reverts commit 443c957b202240dfb30476f912b6bf051aea6dfa.

--- backend/config/database.php
+++ backend/config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -96,7 +96,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require', // <--- ADD THIS LINE
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'sqlsrv' => [
