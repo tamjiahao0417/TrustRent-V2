f27d2826b98cf2034e87495d404ab9d5052bed93
tamjiahao0417 <tamhao@graduate.utm.my>
2026-07-02 13:17:24 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://trustrent-backend-6yww.onrender.com'
-  };
+  production: false,
+  apiUrl: 'https://trustrent-backend-6yww.onrender.com/api'
+};
