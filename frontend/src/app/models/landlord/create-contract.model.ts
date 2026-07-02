7b55f6b5843148a7e4df3a34f03064530843c7aa
tamjiahao0417 <tamhao@graduate.utm.my>
2026-07-03 01:19:04 +0800

    deploy

--- frontend/src/app/models/landlord/create-contract.model.ts
+++ frontend/src/app/models/landlord/create-contract.model.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 // Define the structure of the data you are sending
 export interface ContractPayload {
@@ -16,7 +17,7 @@ export interface ContractPayload {
   providedIn: 'root'
 })
 export class CreateContractModel {
-  private apiUrl = 'http://localhost:8000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- frontend/src/app/models/shared/ai-matching.model.ts
+++ frontend/src/app/models/shared/ai-matching.model.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AiMatchingModel {
-  private apiUrl = 'http://localhost:8000/api';
+  private apiUrl =environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
