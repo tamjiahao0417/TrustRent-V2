95272a8e2734cd0aba70e947c76987dfbc8dd7b2
tamjiahao0417 <tamhao@graduate.utm.my>
2026-07-03 02:23:31 +0800

    Update AiController.php

--- backend/app/Http/Controllers/AiController.php
+++ backend/app/Http/Controllers/AiController.php
@@ -29,7 +29,7 @@ class AiController extends Controller
                 'features' => 'nullable|string'
             ]);
 
-            $apiKey = "AIzaSyAfXyvd4AR1ztwzxEzCYWb5IPkGVzz--RY"; 
+            $apiKey = "AQ.Ab8RN6LPjC4QfIjMDO5X3kkyZJWdZNI8zXdrMmzSjxOzDrSGmg"; 
 
             $prompt = "You are an expert real estate appraiser in Malaysia. Analyze the following property and estimate a fair monthly rental price in RM (Malaysian Ringgit). 
             Property Details:
@@ -109,7 +109,7 @@ class AiController extends Controller
                     'available_properties' => $properties->toArray()
                 ]);
 
-            $apiKey = "AIzaSyAfXyvd4AR1ztwzxEzCYWb5IPkGVzz--RY"; 
+            $apiKey = "AQ.Ab8RN6LPjC4QfIjMDO5X3kkyZJWdZNI8zXdrMmzSjxOzDrSGmg"; 
 
             $response = Http::withoutVerifying()
                 ->timeout(60)
@@ -168,7 +168,7 @@ class AiController extends Controller
                     'available_tenants' => $tenants->toArray()
                 ]);
 
-            $apiKey = "AIzaSyAfXyvd4AR1ztwzxEzCYWb5IPkGVzz--RY"; 
+            $apiKey = "AQ.Ab8RN6LPjC4QfIjMDO5X3kkyZJWdZNI8zXdrMmzSjxOzDrSGmg"; 
 
             $response = Http::withoutVerifying()
                 ->timeout(60)
